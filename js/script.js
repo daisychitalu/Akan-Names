ef5357f6dd14ace8f2c46af20d5bbedddac4bfd0
daisy <daisychitalu20@gmail.com>
2021-06-13 16:42:37 +0300

    add js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var form =document.getElementById("form")
+form.addEventListener('submit',function displayAkanNames(event){
+event.preventDefault()
+let cc = document.getElementById('century').value
+let dd = document.getElementById('day').value
+let mm = document.getElementById('month').value
+let yy = document.getElementById('year').value
+let g = document.getElementById('gender').value
+let maleAkanaNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "kofi", "Kwame"]
+let femaleAkanaNames=["akosua", "Adwoa", "Abenaa", "akua", "yaa", "afua", "ama"]
+
+})
